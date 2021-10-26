3703973d20b4ff247206707053240ebbfbda339e
Robin van der Noord <robinvandernoord@gmail.com>
2021-10-26 21:17:25 +0200

    fixes and improvements: use /opt instead of home

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+set -e
 
 # download flowy executable to ~/.dynwal
 echo "installing flowy"
 mkdir ~/.dynwal/images -p
-cd ~/.dynwal/
+sudo mkdir /opt/dynwal -p
 wget https://github.com/vineetred/flowy/releases/download/0.4.0/flowy_linux_gnu.zip
 unzip flowy_linux_gnu.zip
 rm flowy_linux_gnu.zip
 chmod +x flowy
+sudo mv flowy /opt/dynwal
+
+sudo cp flowy.service /etc/systemd/user/
+systemctl --user enable flowy
+systemctl --user start flowy
 
 # if there is no 'heic' executable, compile it with 'build-heic-js'
-cd -
 echo "installing heic"
-cp convert heic ~/.dynwal/
-echo "Sudo password required to setup file association:"
+sudo cp convert heic /opt/dynwal/
 sudo cp heic.desktop /usr/share/applications/
+# cp heic.desktop ~/.local/share/applications/
+# chmod +x ~/.local/share/applications/heic.desktop
+
 xdg-mime default heic.desktop image/heif
 echo "installation done"
